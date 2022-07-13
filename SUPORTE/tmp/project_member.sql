d2585c6b3177c34e255ae2e0e266ebd154df5a7f
Fyaxi <108303839+Fyaxi@users.noreply.github.com>
2022-07-13 15:52:30 -0300

    ATUALIZAÇÃO SUPORTE

---
+++ SUPORTE/tmp/project_member.sql
@@ -0,0 +1,2 @@
+INSERT INTO project_member (id,project_id,member_id) VALUES (1,1,1);
+INSERT INTO project_member (id,project_id,member_id) VALUES (2,1,4);
---
+++ SUPORTE/tmp/status.sql
@@ -0,0 +1,8 @@
+INSERT INTO status (id,description,final_state,color) VALUES (1,'New','N','#4d993a');
+INSERT INTO status (id,description,final_state,color) VALUES (2,'In progress','N','');
+INSERT INTO status (id,description,final_state,color) VALUES (3,'Waiting feedback','N','#ff8514');
+INSERT INTO status (id,description,final_state,color) VALUES (4,'Resolved','N','');
+INSERT INTO status (id,description,final_state,color) VALUES (5,'Testing','N','');
+INSERT INTO status (id,description,final_state,color) VALUES (6,'Deploy','N','');
+INSERT INTO status (id,description,final_state,color) VALUES (7,'Closed','Y','#5761ba');
+INSERT INTO status (id,description,final_state,color) VALUES (8,'Rejected','Y','#ff1414');
